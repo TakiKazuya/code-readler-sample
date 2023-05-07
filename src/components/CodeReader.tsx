0ac9327e3619d9f66302468d769e7dbf6bfe5248
滝和弥 <kazup0689@gmail.com>
2023-05-07 18:41:45 +0900

    カメラの高さを変更できるようにした

--- src/components/CodeReader.tsx
+++ src/components/CodeReader.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { BarcodeFormat, DecodeHintType, BrowserMultiFormatReader } from '@zxing/library';
 
+type Mode = 'qr' | 'barcode';
+
 const CodeReader: React.FC = () => {
   const [result, setResult] = useState('No result');
+  const [mode, setMode] = useState<Mode>('qr');
   const videoRef = useRef<HTMLVideoElement>(null);
   const hints = new Map();
   const formats = [BarcodeFormat.QR_CODE, BarcodeFormat.CODE_128, BarcodeFormat.EAN_13, BarcodeFormat.EAN_8];
@@ -33,9 +36,15 @@ const CodeReader: React.FC = () => {
     };
   }, [codeReader]);
 
+  const toggleMode = () => {
+    setMode((prevMode) => (prevMode === 'qr' ? 'barcode' : 'qr'));
+  };
+
   return (
     <div>
-      <video ref={videoRef} style={{ width: '100%' }} />
+      <button onClick={toggleMode}>{mode === 'qr' ? 'Switch to Barcode Mode' : 'Switch to QR Code Mode'}</button>
+      <br />
+      <video ref={videoRef} style={{ width: '100%', height: mode === 'qr' ? '100%' : '100px', objectFit: 'none' }} />
       <p>{result}</p>
     </div>
   );
